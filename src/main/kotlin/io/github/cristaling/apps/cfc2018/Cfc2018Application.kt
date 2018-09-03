30fd54c925abad93c5102928f1765ae16e3d2182
Rares Iova <iovarares@gmail.com>
2018-09-03 19:23:07 +0300

    Initial commit

---
+++ src/main/kotlin/io/github/cristaling/apps/cfc2018/Cfc2018Application.kt
@@ -0,0 +1,11 @@
+package io.github.cristaling.apps.cfc2018
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Cfc2018Application
+
+fun main(args: Array<String>) {
+    runApplication<Cfc2018Application>(*args)
+}
---
+++ src/test/kotlin/io/github/cristaling/apps/cfc2018/Cfc2018ApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.github.cristaling.apps.cfc2018
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Cfc2018ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
